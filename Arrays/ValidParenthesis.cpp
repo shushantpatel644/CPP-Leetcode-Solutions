e60c8676b61fb48b23793d799113cfe1a8bf3876
shushantpatel644 <patelshushant695@gmail.com>
2024-02-06 23:58:36 +0530

    feb 6

--- Arrays/ValidParenthesis.cpp
+++ Arrays/ValidParenthesis.cpp
@@ -3,9 +3,12 @@ class Solution {
     stack < int > st;
     int i = 0;
     while (i < s.length()) {
-      // if (s[i] == ')' || s[i] == '}' || s[i] == ']') { 
-      //   st.push(s[i]);}
-         st.push(s[i]);
+    // if (s[i] == ')' || s[i] == '}' || s[i] == ']') { 
+    // st.push(s[i]);
+    //}
+
+      st.push(s[i]);
+
       if (s[i] == ')' && st.size() > 0 && st.top() == '(') {
         st.pop();
       } else if (s[i] == '}' && st.size() > 0 && st.top() == '{') {
